ac715f461fa8ed783ace0acdcbf6d69b65119c51
Azim Krishna <azimbaji19@gmail.com>
2025-03-03 18:42:20 +0530

    removed condition for the email being verified to login to panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,7 @@ class User extends Authenticatable implements FilamentUser
     
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@quantastic.in') && $this->hasVerifiedEmail();
+        // return str_ends_with($this->email, '@quantastic.in') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@quantastic.in');
     }
 }
