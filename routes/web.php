a9e3fdb049ac86625b39705c22149803f6f5ab22
anshika282 <mandyalanshika@gmail.com>
2025-03-06 18:12:14 +0530

    route to change publish date and style tag fixes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\PageController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/update-slug',[BlogController::Class,'updatePublishedDates']);
 
 Route::get('/product-page/{slug}', [PageController::class,'getProductPage']);
 Route::get('/{slug?}', [PageController::class,'getPage'])->where('slug', '.*');
