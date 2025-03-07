b31aafeaf92d6c1e82ea32e479ed9aae5d701fa4
anshika282 <mandyalanshika@gmail.com>
2025-03-07 15:48:28 +0530

    added event to change temp acc to slug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Pages;
+use App\Observers\PageObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Pages::observe(PageObserver::class);
     }
 }
