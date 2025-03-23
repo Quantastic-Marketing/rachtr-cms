d2c2307d6de906b2b295def8325c9b3faa3c4f46
anshika282 <mandyalanshika@gmail.com>
2025-03-23 23:41:38 +0530

    chngs for optimisation

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -36,18 +36,25 @@ class PageController extends Controller
                 }
 
                 $products = collect();
-                if (!empty($pageDetails->content['sections'])) {
-                    $productIds = collect($pageDetails->content['sections'])->pluck('products')->flatten()->unique();
 
-                    $products =  Product::whereIn('id', $productIds)
-                    ->select([
-                        'id',
-                        'name',
-                        'slug',
-                        'content->product_desc as product_desc',
-                        'content->product_images as product_images'
-                    ])
-                    ->get();
+                if (!empty($pageDetails->content['sections'])) {
+                    $productIds = collect($pageDetails->content['sections'])
+                        ->pluck('products')
+                        ->flatten()
+                        ->unique()
+                        ->filter();
+                
+                    if ($productIds->isNotEmpty()) {
+                        $products = Product::whereIn('id', $productIds)
+                            ->select([
+                                'id',
+                                'name',
+                                'slug',
+                                'content->product_desc as product_desc',
+                                'content->product_images as product_images'
+                            ])
+                            ->lazy();
+                    }
                 }
                 
                 return view("layouts.app",['page' => $pageDetails,'templatePath'=>$templatePath,'products' => $products]);
