97f3d4c31d28089cbb10a0c99f63e680eb74de65
anshika282 <mandyalanshika@gmail.com>
2025-03-24 01:15:40 +0530

    optimize the code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,78 +4,100 @@
 <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-        <title>{{ $page->seo->title ?? config('app.name')}}</title>
-        <meta name="description" content="{{ $page->seo->description ?? 'Rahctr' }}">
-        <meta name="author" content="{{ $page->seo->author ?? 'Rachtr' }}">
+
         @php
-            $metaData = json_decode($page->seo->meta ?? '{}', true);
+            $seo = $page->seo ?? (object)[];
+            $seoTitle = $seo->title ?? config('app.name');
+            $seoDescription = $seo->description ?? 'Rachtr';
+            $seoAuthor = $seo->author ?? 'Rachtr';
+            $canonicalUrl = $seo->canonical_url ?? url()->current();
+            $seoImage = $seo->image ?? asset('images/default-image.jpg');
+
+            $metaData = json_decode($seo->meta ?? '{}', true);
             $focusKeywords = isset($metaData['focus_keywords']) ? implode(', ', $metaData['focus_keywords']) : 'rachtr';
+
+            $currentPath = request()->path();
         @endphp
-        <meta name="keywords" content="{{ $focusKeywords }}">
-        <link rel="canonical" href="{{ $page->seo->canonical_url ?? url()->current() }}">
-        <!-- Open Graph (Facebook, LinkedIn) -->
-        <meta property="og:title" content="{{ $page->seo->title ?? config('app.name') }}">
-        <meta property="og:description" content="{{ $page->seo->description ?? 'Rahctr' }}">
-        <meta property="og:image" content="{{ isset($page->seo->image) ? asset($page->image) : asset('images/default-image.jpg') }}">
-        <meta property="og:url" content="{{ $page->seo->canonical_url ?? url()->current() }}">
-        <meta property="og:type" content="website">
+        <title>{{ $seoTitle }}</title>
+            <meta name="description" content="{{ $seoDescription }}">
+            <meta name="author" content="{{ $seoAuthor }}">
+            <meta name="keywords" content="{{ $focusKeywords }}">
+            <link rel="canonical" href="{{ $canonicalUrl }}">
 
-        <!-- Twitter Card -->
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="{{ $page->seo->title ?? config('app.name') }}">
-        <meta name="twitter:description" content="{{ $page->seo->description ?? 'Rahctr' }}">
-        <meta name="twitter:image" content="{{ isset($page->seo->image) ? asset($page->image) : asset('images/default-image.jpg') }}">
+            <!-- Open Graph (Facebook, LinkedIn) -->
+            <meta property="og:title" content="{{ $seoTitle }}">
+            <meta property="og:description" content="{{ $seoDescription }}">
+            <meta property="og:image" content="{{ $seoImage }}">
+            <meta property="og:url" content="{{ $canonicalUrl }}">
+            <meta property="og:type" content="website">
 
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!--<link rel="stylesheet" href="css/hover-min.css" type="text/css">-->
-        <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}" type="text/css">
-        <link rel="stylesheet" href="{{ asset('css/slick.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.min.css') }}" />
-        <link href="{{ asset('css/bootstrap.min.css') }}" type="text/css" rel="stylesheet">
-        <link  href="{{ asset('css/style.css') }}" type="text/css" rel="stylesheet">
-        <link href="{{ asset('css/responsive.css') }}" type="text/css" rel="stylesheet">
-            @if(request()->is('*product-page*'))
-                <link rel="stylesheet" href="{{ asset('css/product-template.css') }}" type="text/css">
-            @endif
-            @if(request()->is('*architect-center'))
-            <link rel="stylesheet" href="{{ asset('css/architect.css') }}">
-            @endif
-            @if(request()->is('*contractor-center'))
-            <link  href="{{ asset('css/puFloor.css') }}" type="text/css" rel="stylesheet">
-            <link  href="{{ asset('css/contractual.css') }}" type="text/css" rel="stylesheet">
-            <link rel="stylesheet" href="{{ asset('css/architect.css') }}">
-            @endif
-        @if(request()->is('*about-us') || request()->is('*support-center'))
-        <link href="{{ asset('css/aboutus.css') }}" type="text/css" rel="stylesheet">
-        @endif
-        @if(request()->is('*upload-cv'))
-        <link rel="stylesheet" href="{{ asset('css/uploadCV.css') }}">
-        @endif
-        @if(request()->is('*careers'))
-            <link rel="stylesheet" href="{{ asset('css/careersStyle.css') }}">
-        @endif
-        @if(request()->is('*contact-us'))
-        <link  href="{{ asset('css/puFloor.css') }}" type="text/css" rel="stylesheet">
-        <link href="css/contractual.css" type="text/css" rel="stylesheet">
-        <link href="css/contact.css" type="text/css" rel="stylesheet">
-        @endif
-        @if(!empty($page->content['is_product_list']) && $page->content['is_product_list'] == 1)
-            <link rel="stylesheet" href="{{ asset('css/epoxyIndustry.css') }}">
-        @endif
-        @if(request()->is('*pu-flooring'))
-        <link  href="{{ asset('css/epoxy.css') }}" type="text/css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/epoxyCost.css') }}">
-        <link  href="{{ asset('css/puFloor.css') }}" type="text/css" rel="stylesheet">
-        @endif
-        @if(request()->is('*epoxy-flooring'))
-        <link  href="{{ asset('css/epoxy.css') }}" type="text/css" rel="stylesheet">
-        @endif
-        @if(request()->is('*epoxy-flooring-cost-price'))
-        <link  href="{{ asset('css/epoxy.css') }}" type="text/css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/epoxyCost.css') }}">
-        @endif
+            <!-- Twitter Card -->
+            <meta name="twitter:card" content="summary_large_image">
+            <meta name="twitter:title" content="{{ $seoTitle }}">
+            <meta name="twitter:description" content="{{ $seoDescription }}">
+            <meta name="twitter:image" content="{{ $seoImage }}">
+
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" as="style">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <!--<link rel="stylesheet" href="css/hover-min.css" type="text/css">-->
+            <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}" type="text/css">
+            <link rel="stylesheet" href="{{ asset('css/slick.css')}}" type="text/css">
+            <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.min.css') }}" />
+            <link href="{{ asset('css/bootstrap.min.css') }}" type="text/css" rel="stylesheet">
+            <link  href="{{ asset('css/style.css') }}" type="text/css" rel="stylesheet">
+            <link href="{{ asset('css/responsive.css') }}" type="text/css" rel="stylesheet">
+            @php
+                $styles = [];
+                switch (true) {
+                    case str_contains($currentPath, 'product-page'):
+                        $styles[] = 'css/product-template.css';
+                        break;
+                    case str_contains($currentPath, 'architect-center'):
+                        $styles[] = 'css/architect.css';
+                        break;
+                    case str_contains($currentPath, 'contractor-center'):
+                        $styles[] = ['css/puFloor.css', 'css/contractual.css', 'css/architect.css'];
+                        break;
+                    case str_contains($currentPath, 'about-us') || str_contains($currentPath, 'support-center'):
+                        $styles[] = 'css/aboutus.css';
+                        break;
+                    case str_contains($currentPath, 'upload-cv'):
+                        $styles[] = 'css/uploadCV.css';
+                        break;
+                    case str_contains($currentPath, 'careers'):
+                        $styles[] = 'css/careersStyle.css';
+                        break;
+                    case str_contains($currentPath, 'contact-us'):
+                        $styles[] = ['css/puFloor.css', 'css/contractual.css', 'css/contact.css'];
+                        break;
+                    case !empty($page->content['is_product_list']) && $page->content['is_product_list'] == 1:
+                        $styles[] = 'css/epoxyIndustry.css';
+                        break;
+                    case str_contains($currentPath, 'pu-flooring'):
+                        $styles[] = ['css/epoxy.css', 'css/epoxyCost.css', 'css/puFloor.css'];
+                        break;
+                    case str_contains($currentPath, 'epoxy-flooring'):
+                        $styles[] = 'css/epoxy.css';
+                        break;
+                    case str_contains($currentPath, 'epoxy-flooring-cost-price'):
+                        $styles[] = ['css/epoxy.css', 'css/epoxyCost.css'];
+                        break;
+                }
+            @endphp
+
+            @foreach ((array)$styles as $style)
+                @if(is_array($style))
+                    @foreach ($style as $s)
+                        <link rel="stylesheet" href="{{ asset($s) }}">
+                    @endforeach
+                @else
+                    <link rel="stylesheet" href="{{ asset($style) }}">
+                @endif
+            @endforeach
+
         <script type="application/ld+json">{"@context":"https://schema.org","@type":"Organization","name":"Rachtr","alternateName":"Rachtr","url":"https://www.rachtr.com/","logo":"https://static.wixstatic.com/media/386348_1185cc80ad414f0b866b359f72e3844b~mv2.png/v1/fill/w_108,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Asset%201%20copy.png","sameAs":["https://www.instagram.com/rachtr_/","https://www.facebook.com/rachtr1","https://www.linkedin.com/company/13608620/admin/dashboard/","https://www.youtube.com/@rachtrchemicals7185"]}</script>
+        
         @if (!empty($page->schema_data) && is_array($page->schema_data))
             @foreach ($page->schema_data as $schemaItem)
                 @if (isset($schemaItem['schema']))
@@ -99,14 +121,16 @@
             @endif
 
             @include('CommonTemplates.footerHome')
-            <script src="{{ asset('js/jquery.min.js') }}" type="text/jscript"></script>
-            <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
-            <script src="{{ asset('js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
-            <script src="{{ asset('js/slick.js') }}"></script>
-            <script src="{{ asset('js/jquery.fancybox.min.js') }}"></script>
-            <script src="{{ asset('js/custom.js') }}"></script>
+            <script defer src="{{ asset('js/jquery.min.js') }}" type="text/jscript"></script>
+            <script defer src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
+            <script defer src="{{ asset('js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+            @if(empty($page->content['is_product_list']))
+                <script defer src="{{ asset('js/slick.js') }}"></script>
+                <script defer src="{{ asset('js/jquery.fancybox.min.js') }}"></script>
+                <script defer src="{{ asset('js/custom.js') }}"></script>
+            @endif
             
-            <script type="text/javascript">
+            <script defer type="text/javascript">
                 $('#showLeft').click(function(){
                 $(this).toggleClass('open');
                 $(".nav").slideToggle();
