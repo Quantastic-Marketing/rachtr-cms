3bb9cade01e12572064165bc5758a7ed8d8c7148
anshika282 <mandyalanshika@gmail.com>
2025-04-07 18:33:39 +0530

    added panel for redirects

--- routes/web.php
+++ routes/web.php
@@ -10,32 +10,22 @@ use App\Http\Controllers\ProductController;
 //     return view('welcome');
 // });
 Route::get('/update-slug',[BlogController::Class,'updatePublishedDates']);
-Route::redirect('product-page/rachtr-xc-3', '/product-page/rachtr-xc-4', 301);
 Route::get('/product-page/{slug}', [PageController::class,'getProductPage'])->name('product.page');
 Route::get('/sitemap.xml', [PageController::class, 'getSiteMap']);
 Route::post('/contact-submit', [FormController::class, 'addContactDetail'])->name('contact');
 Route::post('/connect', [FormController::class, 'addConnectDetail'])->name('connect');
 Route::post('/cvform', [FormController::class, 'addCvDetail'])->name('cvform');
 Route::post('/submit-epoxy-form', [FormController::class, 'addEpoxyDetail'])->name('epoxyForm');
-Route::redirect('/category/products','/category/product',301);
 Route::get('/category/{slug}',[ProductController::class,'getAllProducts'])->name('all-products');
 Route::get('/product-lists', [ProductController::class, 'index'])->name ('product-lists');
 Route::get('/search/products', [ProductController::class, 'loadMoreProducts'])->name ('search-product');
 Route::get('/publish-blogs',[PageController::class, 'publishPendingPosts'] );
-Route::redirect('/post', '/blogs', 301);
-Route::redirect('/post/solutions','/blogs/white-marble-installation-methodology',301);
-Route::redirect('/post/epoxyflooringforextremetemperatures','/blogs/cold-storage-refrigeration-facilities-epoxy-flooring-for-extreme-temperatures',301);
 Route::get('/post/{slug}', function ($slug) {
     return redirect("/blogs/{$slug}", 301);
 });
-Route::redirect('/blog', '/blogs', 301);
 Route::get('/blog/page/{page}', function ($page) {
     return redirect()->to("/blogs?page={$page}", 301);
 });
-Route::redirect('/support-center/architect-center', '/architect-center', 301);
-Route::redirect('/support-center/contractor-center', '/contractor-center', 301);
-Route::redirect('/epoxy-flooring', '/industrial-flooring-solutions/epoxy-flooring-services', 301);
-Route::redirect('/epoxy-flooring-cost-price', '/industrial-flooring-solutions/epoxy-flooring-cost-price', 301);
 Route::get('/{slug?}', [PageController::class,'getPage'])->where('slug', '.*');
 Route::fallback(function () {
     return response()->view('fallback', [], 404);
