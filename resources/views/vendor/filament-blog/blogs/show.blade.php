b7b80bca3f5ac059eef316ea6d819a1746ff61f0
anshika282 <mandyalanshika@gmail.com>
2025-04-16 12:44:07 +0530

    chng blog var from pagr to post

--- resources/views/vendor/filament-blog/blogs/show.blade.php
+++ resources/views/vendor/filament-blog/blogs/show.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
         @php
-            $seo = $page->seoDetail ?? (object)[];
+            $seo = $post->seoDetail ?? (object)[];
             $seoTitle = $seo->title ?? config('app.name');
             $seoDescription = $seo->description ?? 'Rachtr';
             $seoAuthor = $seo->author ?? 'Rachtr';
