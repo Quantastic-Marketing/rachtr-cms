0a8cb0df0431040f660aef963fbae26c81f5798b
anshika282 <mandyalanshika@gmail.com>
2025-05-09 14:41:01 +0530

    chnged the blogs url to app config in search

--- resources/views/CommonTemplates/headerHome.blade.php
+++ resources/views/CommonTemplates/headerHome.blade.php
@@ -73,7 +73,7 @@
                                 </li>
                               @endforeach
                                 <li class="pe-0 position-relative" >
-                                  <div class="search-container position-relative " id="searchWrap">
+                                  <div class="search-container position-relative " id="searchWrap" data-app-url="{{ config('app.url') }}">
                                     <form action="{{ route('product-lists')}}"  method="GET">
                                       <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="5 5 14 14"><path fill="currentColor" d="m15.683 14.6 3.265 3.265a.2.2 0 0 1 0 .282l-.8.801a.2.2 0 0 1-.283 0l-3.266-3.265a5.961 5.961 0 1 1 1.084-1.084zm-4.727 1.233a4.877 4.877 0 1 0 0-9.754 4.877 4.877 0 0 0 0 9.754z"></path></svg></span>
                                       <input type="text" id="searchInput" placeholder="Search" name="query" x-model.debounce.500ms="query" value="{{ request('query') }}" >
