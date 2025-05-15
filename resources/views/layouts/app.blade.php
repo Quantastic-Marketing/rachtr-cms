93087a6eae3085365b7bade0223dd40a1d5cfed7
anshika282 <mandyalanshika@gmail.com>
2025-05-16 04:03:31 +0530

    updated the favicon in the public folder insyead of iumages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,7 +104,7 @@
                 @endif
             @endforeach
         @endif
-        <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon_img.ico') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
         <!-- Google Tag Manager -->
         <script async defer>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
